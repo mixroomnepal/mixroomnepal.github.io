06fa16d19783e22b62f346907d4db5402644ae53
mixroomnepal <64257102+mixroomnepal@users.noreply.github.com>
2020-04-25 13:25:51 +0545

    php fixes

--- forms/contact.php
+++ forms/contact.php
@@ -1,13 +1,3 @@
 <?php
-$to = 'antim.timsina7777@gmail.com';
-$subject = 'Marriage Proposal';
-$message = 'Hi Jane, will you marry me?'; 
-$from = 'peterparker@email.com';
- 
-// Sending email
-if(mail($to, $subject, $message)){
-    echo 'Your mail has been sent successfully.';
-} else{
-    echo 'Unable to send email. Please try again.';
-}
+echo "My first PHP script!";
 ?>
