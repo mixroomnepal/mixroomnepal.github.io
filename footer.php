d825b34326f1238f82f23dabb17a90a01c86400e
mixroomnepal <64257102+mixroomnepal@users.noreply.github.com>
2020-04-28 09:00:58 +0545

    footer php added

---
+++ footer.php
@@ -0,0 +1,178 @@
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="row footer-content">
+     
+
+      <!--------------------------------------------------FIRST COLUMN--------------------------------------------------------->
+      <div class="col-lg-4 col-md-6 col-sm-12 footer-column">
+      
+          <!-- Logo in footer  -->
+          <div clas="mixroom-logo">
+            <a href="#">
+            <img class="footer-logo" src="assets/img/music_images/studio_images/LOGO.PNG"></img>
+          </a>
+          </div>
+    
+          <!-- Social icons in footer  -->
+          <div class="footer-social-section">
+            <span class="social-title"><h4>Social</h4></span>
+            <a href="#" target="_blank"><i class='footer-social-icon bx bxl-facebook'></i></a>
+            <a href="#" target="_blank"><i class='footer-social-icon bx bxl-instagram' ></i></a>
+            <a href="#" target="_blank"> <i class='footer-social-icon bx bxl-youtube' ></i></a>
+            <a href="#" target="_blank"><i class='footer-social-icon bx bxl-soundcloud' ></i></a>
+            <a href="#" target="_blank"><i class='footer-social-icon bx bxl-spotify' ></i></a>
+          </div>
+
+
+          <!-- About in footer  -->
+          <div class="footer-about-section">
+            <h4>About Mix Room</h4>
+            <div class="footer-about-content">Mix Room was created, crafted and designed for Nepalese Music Fraternity to experience next level of Music and Audio production.</div>
+          </div>
+
+
+          <!-- Address info in footer  -->
+          <div class="footer-address-section">
+            <h4>Address</h4>
+            <strong><span class="place-name"></span></strong>
+            <br>
+            <span class="place-location"></span>
+            <p style="padding-top:10px;">
+              <a class="venobox" data-vbtype="iframe" href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3571.6632788387237!2d87.2752812151076!3d26.46658078552418!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ef758fcfb6c9dd%3A0xe60b62f04a77a2a6!2sMix%20Room!5e0!3m2!1sen!2snp!4v1587796957148!5m2!1sen!2snp" target="_blank">
+                <i class="icofont-google-map"></i> How to get here?
+              </a>
+            </p>
+          </div>
+        
+
+          <!-- Review in footer  -->
+          <div class="review-section">
+            <h4>Reviews</h4>
+            <div id="google-reviews"></div>
+            <img class="google-logo" src="assets/img/google-logo.png"></img>
+            <span class="place-name"></span>
+            <div class="stars-outer">
+              <div class="stars-inner">
+              </div>
+            </div>
+            <div class="review-text">
+              <span class="review-average"></span> 
+              Stars - Based on <a href="https://www.google.com/maps/place/Mix+Room/@26.4665808,87.2752812,17z/data=!4m7!3m6!1s0x39ef758fcfb6c9dd:0xe60b62f04a77a2a6!8m2!3d26.466576!4d87.2774699!9m1!1b1" 
+              class="review-link" title="User Reviews">
+                <span class="total-reviews"></span> 
+                User Reviews</a>
+              </div>
+          </div>   
+        
+      </div>
+
+     <!--------------------------------------------------SECOND COLUMN------------------------------------------------------------------------>
+      <div class="col-lg-3 col-md-6 footer-column">
+
+        <!-- Services section-->
+        <div class="footer-services">
+          <h4>Mix Room Services</h4>
+          <ul>
+            <a href="#" target='_blank'><li>Studios</li></a>
+            <a href="#" target='_blank'><li>Online Mixing</li></a>
+            <a href="#" target='_blank'><li>Online Mastering</li></a>
+            <a href="#" target='_blank'><li>Mix Room Academy</li></a>
+            <a href="#" target='_blank'><li>Acoustic Solutions</li></a>
+            <a href="#" target='_blank'><li>Music Publishing</li></a>
+            <a href="#" target='_blank'><li>Film Making and Music Videos</li></a>
+            <a href="#" target='_blank'><li>IT Solutions and Web Design</li></a>
+          </ul>
+        </div>
+
+         <!-- Book section-->
+
+         <div class="footer-book">
+           <h4> Book Our Services</h4>
+           <ul>
+            <a href="#" target='_blank'><li>Book Studio</li></a>
+            <a href="#" target='_blank'><li>Book Mixing</li></a>
+            <a href="#" target='_blank'><li>Book Mastering</li></a>
+            <a href="#" target='_blank'> <li>Enroll in Academy</li></a>
+            <a href="#" target='_blank'> <li>Book Solutions</li></a>
+          </ul>
+         </div>
+
+        <!--Contact section-->
+
+        <div class="footer-contact">
+          <h4>Contact us</h4>
+          <i class='bx bx-mail-send'></i>
+          enquiry.mixroom@gmail.com
+          <br>
+          <br>
+          <i class='bx bxs-phone'></i> 
+          <span class="place-phone"></span>
+        </div>
+
+
+      </div>
+
+      <!--------------------------------------------------THIRD COLUMN------------------------------------------------------------------------>
+      <div class="col-lg-5 col-md-12 col-sm-12 footer-column">
+        
+        <!--News section-->
+        <div class="footer-news">
+          <h4>Recent News</h4>
+           <div class="footer-news-content">
+
+            <!----------First News---------->
+             <div class="news-from-database">
+               
+                      <div class="news-first-col">
+                        <img class="news-image" src="https://picsum.photos/120?random=1" alt="News_image"></img>
+                      </div>        
+                      <div class="news-second-col">
+                        <h5 class="news-title">Online Mixing – Pay it Forward</h5>
+                        <p class="time-content">2 minutes ago</p>
+                        <p class="news-content">At Studios 301 we acknowledge the significant impact that COVID-19 has had, and will continue to have on the creative industries …</p>
+                      </div>
+                
+             </div>
+            <!----------Second News---------->
+            <div class="news-from-database">
+               
+              <div class="news-first-col">
+                <img class="news-image" src="https://picsum.photos/120?random=2" alt="News_image"></img>
+              </div>        
+              <div class="news-second-col">
+                <h5 class="news-title">Online Mixing – Pay it Forward</h5>
+                <p class="time-content">2 minutes ago</p>
+                <p class="news-content">At Studios 301 we acknowledge the significant impact that COVID-19 has had, and will continue to have on the creative industries …</p>
+              </div>
+        
+     </div>
+            <!----------Third News---------->
+            <div class="news-from-database">
+                    <div class="news-first-col">
+                      <img class="news-image" src="https://picsum.photos/120?random=3" alt="News_image"></img>
+                    </div>        
+                    <div class="news-second-col">
+                      <h5 class="news-title">2020 Queensland Music Awards Winners</h5>
+                      <p class="time-content">3 months ago</p>
+                      <p class="news-content">Congratulations to all of our nominated clients, engineers and winners at 2020 Queensland Music Awards which was held at Brisbane’s Fortitude Music Hall on March 3rd.</p>
+                    </div>
+            </div>
+             
+
+           </div> 
+        </div>
+
+      </div>
+    
+    </div>
+  
+    <div class="container footer-bottom clearfix">
+      <div class="copyright">
+        &copy; All Rights Reserved - Mix Room Nepal
+      </div>
+      <div class="credits">
+        Designed by <a href="https://bootstrapmade.com/">Animesh</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
